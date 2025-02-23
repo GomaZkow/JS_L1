1af4e0fa5f1432f8aabee3e4c3992c456c3386b0
GomaZkow <opypartur@gmail.com>
2025-02-23 19:12:32 +0300

    удалил комы+взял кубический корень

--- Zad1.js
+++ Zad1.js
@@ -1,21 +1,13 @@
-//1729 --> true
-// 1729 = 9³ + 10³
-// = a³ + b³
-//n = a³
-//+ b³
-// console.log(firstCube);
 
-//81
 function hasTwoCubeSums(n) {
-  //Math.cbrt(n);
-  let NumSbrt = n;
+  let NumSbrt = Math.cbrt(n);
   let count = 0;
 
   for (let firstCube = 0; firstCube <= NumSbrt; firstCube++) {
     for (let secondCube = 0; secondCube <= NumSbrt; secondCube++) {
       if (
         firstCube != secondCube &&
-        Math.pow(secondCube, 3) + Math.pow(firstCube, 3) == NumSbrt
+        Math.pow(secondCube, 3) + Math.pow(firstCube, 3) == n
       ) {
         count++;
 
@@ -23,7 +15,7 @@ function hasTwoCubeSums(n) {
           for (let fourthCube = 0; fourthCube <= NumSbrt; fourthCube++) {
             thirdCube++;
             if (
-              Math.pow(thirdCube, 3) + Math.pow(fourthCube, 3) == NumSbrt &&
+              Math.pow(thirdCube, 3) + Math.pow(fourthCube, 3) == n &&
               thirdCube != fourthCube &&
               firstCube != thirdCube &&
               firstCube != fourthCube &&
@@ -40,5 +32,4 @@ function hasTwoCubeSums(n) {
   }
   return false;
 }
-
 console.log(hasTwoCubeSums(42));
